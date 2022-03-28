3ae81bcb4159011e6328e79a17a0d3eb03bf2633
Timothy Thomas <inn-tanner0r@icloud.com>
2022-03-28 03:23:36 -0700

    Initial Commit

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,7 +3,7 @@ from flask_bcrypt import Bcrypt #bcrypt is handled in the controllers (Models)
 from flask import Flask,flash
 import re
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 #👇 We need this for session/flash
-app.secret_key = "shhhhhhhhhhh!"
+application.secret_key = "shhhhhhhhhhh!"
