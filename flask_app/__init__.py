700740ef4b45901eaa8deea8e7ec3c75e4ef4c52
Timothy Thomas <inn-tanner0r@icloud.com>
2022-03-28 23:48:12 -0700

    update

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,7 +3,7 @@ from flask_bcrypt import Bcrypt #bcrypt is handled in the controllers (Models)
 from flask import Flask,flash
 import re
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 #👇 We need this for session/flash
-application.secret_key = "shhhhhhhhhhh!"
+app.secret_key = "shhhhhhhhhhh!"
--- server.py
+++ server.py
@@ -3,3 +3,4 @@ from flask_app.controllers import users, recipes
 
 if __name__ == "__main__":
     app.run(debug=True, port = 5001)
+    
